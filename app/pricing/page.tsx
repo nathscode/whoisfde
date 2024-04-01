0acef6e6a9436853f0ac4543aa758446110cf83d
nathscode <michaelnathrm@gmail.com>
2024-04-01 08:16:33 +0100

    price page updage

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,12 +1,11 @@
-import Pricing from "@/components/Pricing";
-import React from "react";
+import PricingSection from "@/components/PricingSection";
 
 type Props = {};
 
 const PricingPage = (props: Props) => {
 	return (
 		<div className="flex flex-col justify-center items-center py-20">
-			<Pricing />
+			<PricingSection />
 		</div>
 	);
 };
